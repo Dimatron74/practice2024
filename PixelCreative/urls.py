a899b5fca0c3fe0e3d741445ec82737762655304
Dimatron74 <logachev_16@bk.ru>
2024-07-16 13:27:41 +0300

    Change name project

--- PixelCreative/urls.py
+++ PixelCreative/urls.py
@@ -3,10 +3,6 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-# from django.urls import re_path as url
-# from django.conf import settings
-# from django.views.static import serve
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("__reload__/", include("django_browser_reload.urls")),
